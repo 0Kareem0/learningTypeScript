8f7896212c88a4b6194169b06fc46fd998d69c7c
Elite <kimo123kimo321@gmail.com>
2026-07-02 09:19:06 +0300

    void and never ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   console.log(Math.floor(10.22));
-  console.log("optional parameters");
+  console.log("void and never ✅");
   
   return (
     <div className="flex justify-center items-center h-screen">
