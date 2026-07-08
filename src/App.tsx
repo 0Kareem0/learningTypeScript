6ab4a8fc1b6f4571510db973c95ede339a052f76
Elite <kimo123kimo321@gmail.com>
2026-07-08 15:49:21 +0300

    HTMLImageElement ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 export default function App() {
   console.log(Math.floor(10.22));
-  console.log("Built-in Interface ✅");
+  console.log("HTMLImageElement ✅");
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-3xl font-bold underline text-white">
