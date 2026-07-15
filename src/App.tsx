daee179073743022ef2d0c56d2327ae9c6b71d33
Elite <kimo123kimo321@gmail.com>
2026-07-16 01:35:14 +0300

    Generics Multiple Types ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 export default function App() {
   console.log(Math.floor(10.22));
-  console.log("Chapter 12 Generics");
+  console.log("Generics Multiple Types ✅");
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-3xl font-bold underline text-white">
