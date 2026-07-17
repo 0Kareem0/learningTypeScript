86a1282d7041ca9853e2879afc173b5a633b970d
Elite <kimo123kimo321@gmail.com>
2026-07-17 21:42:43 +0300

    simple Generic function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 
 export default function App() {
   console.log(Math.floor(10.22));
-  console.log("not much but still moving");
+  console.log("simple Generic Function");
+  function firstFunc<T>(value:T):T{
+  return value
+}
+console.log(firstFunc<number>(100));
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-3xl font-bold underline text-white">
         Hello world! typeScript incoming
       </h1>
+      
     </div>
   )
 }
