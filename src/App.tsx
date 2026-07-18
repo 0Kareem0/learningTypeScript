3d03f5b324a94caad5208a3453f2ba0e195592fa
Elite <kimo123kimo321@gmail.com>
2026-07-18 17:06:01 +0300

    Generics Classes ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 export default function App() {
   console.log(Math.floor(10.22));
-  console.log("whatimdoing");
+  console.log("Generics Classes ✅");
   function firstFunc<T>(value:T):T{
   return value
 }
