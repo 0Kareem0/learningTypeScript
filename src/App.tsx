6cad54bfcba46b19849ff2ed872f4cde6f85db92
Elite <kimo123kimo321@gmail.com>
2026-07-26 06:04:41 +0300

    null handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Cards from "./components/Cards"
 import Alert from "./components/Alert"
+import { useState , useEffect } from "react"
 export default function App() {
   console.log("third session ✅");
   function firstFunc<T>(value:T):T{
   return value
 }
+type User = {
+  name: string;
+  age: number;
+}
+
+      const [user, setUser] = useState<User | null>(null)
+      const [loading, setLoading] = useState(true)
+
+        useEffect(()=>{
+        setUser({name:"Kareem", age:25})
+        setLoading(false)
+       },[]) 
 
       function alertMessage(message:string) {
         alert(message)
@@ -25,6 +38,7 @@ console.log(firstFunc<number>(100));
       {users.map((user) => (
         <Cards key={user.name} color="green" title={user.name} description={user.age.toString()} />
       ))}
+        {!loading && <p>{user?.name}</p>}
     </div>
   )
 }
